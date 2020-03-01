bb558b81fd9c8185bfb7cd6ca9613d0c505c734f
Kaick Garcia <kaickgarciasjc@gmail.com>
2020-03-01 15:41:09 -0300

    Adiciona Banco de Dados

--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const routes = require('./routes');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://josephds:Maisvc10@cluster0-y33pg.mongodb.net/omnistack?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}, () => {console.log("Conectado ao banco de dados")})
+mongoose.connect(
+  "mongodb+srv://Inspire_Store:kaick321@cluster0-xbl0b.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log("Conectado ao banco de dados");
+  }
+);
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(process.env.PORT || 4200, () =>  console.log("Running on port 4200"));
+app.listen(process.env.PORT || 4200, () => console.log("Running on port 4200"));
